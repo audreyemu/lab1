6ab3e355effb582858ea324a55a6cd04ff6a07e2
Audrey Emis <mvp24601g@gmail.com>
2024-01-18 07:13:20 -0800

    Testing

--- pipe.c
+++ pipe.c
@@ -3,27 +3,34 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <errno.h>
 
 int main(int argc, char *argv[])
 {
   int to_shell[2];
   pipe(to_shell);
   int return_code = fork();
+  
   for(int i = 1; i < argc; i++){
-    if(i == 1){
+    if(i == 1 && i != argc-1){
       if(return_code == 0){
 	dup2(to_shell[1],1);
 	close(to_shell[0]);
-	execlp(argv[i], argv[i], NULL);
+	exec_status = execlp(argv[i], argv[i], NULL);
       }
-      else{
+      else if(return_code > 0){
 	close(to_shell[1]);
 	int pid = return_code;
 	int status = 0;
 	waitpid(pid, &status, 0);
+	if(WIFEXITED(status) && WEXITSTATUS(status) != 0){
+	  exit(errno);
+	}
 	pipe(to_shell);
 	return_code = fork();
-	//close(3);
+      }
+      else{
+	exit(1);
       }
     }
     else if(i == argc-1){
@@ -31,12 +38,21 @@ int main(int argc, char *argv[])
 	dup2(3, 0);
 	close(to_shell[0]);
 	close(to_shell[1]);
-	execlp(argv[i], argv[i], NULL);
+	exec_status = execlp(argv[i], argv[i], NULL);
       }
-      else{
+      else if(return_code > 0){
+	int pid = return_code;
+	int status = 0;
+	waitpid(pid, &status, 0);
+	if(WIFEXITED(status) && WEXITSTATUS(status) != 0){
+	  exit(errno);
+	}
 	close(3);
 	close(to_shell[0]);
 	close(to_shell[1]);
+      }
+      else{
+	exit(1);
       }
     }
     else{
@@ -44,22 +60,26 @@ int main(int argc, char *argv[])
 	dup2(3, 0);
 	dup2(to_shell[1], 1);
 	close(to_shell[0]);
-	execlp(argv[i], argv[i], NULL);
+	exec_status = execlp(argv[i], argv[i], NULL);
       }
-      else{
+      else if(return_code > 0){
 	close(3);
 	close(to_shell[1]);
 	dup2(4, 3);
 	int pid = return_code;
 	int status = 0;
 	waitpid(pid, &status, 0);
+	if(WIFEXITED(status) && WEXITSTATUS(status) != 0){
+	  exit(errno);
+	}
 	pipe(to_shell);
 	return_code = fork();
-	//close(3);
+      }
+      else{
+	exit(1);
       }
     }
   }
-  printf("Done done done\n");
       
   return 0;
 }

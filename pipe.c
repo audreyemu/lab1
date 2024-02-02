74a48fae7a00b594267ab101ad7789d00c5cb53b
audreyemu <121738351+audreyemu@users.noreply.github.com>
2024-02-02 12:09:55 -0800

    Commented pipe.c

--- pipe.c
+++ pipe.c
@@ -8,81 +8,81 @@
 int main(int argc, char *argv[])
 {
   if(argc == 1){
-    return EINVAL;
+    return EINVAL; // if only one argument (just pipe) return error
   }
   int to_shell[2];
   pipe(to_shell);
-  int return_code = fork();
+  int return_code = fork(); // create initial pipe and fork
   
-  for(int i = 1; i < argc; i++){
-    if(i == 1 && i != argc-1){
-      if(return_code == 0){
-	dup2(to_shell[1],1);
+  for(int i = 1; i < argc; i++){ // for each argument
+    if(i == 1 && i != argc-1){ // if it's the first one and not the last
+      if(return_code == 0){ // if it's the child
+	dup2(to_shell[1],1); // redirect output to write end of pipe
 	close(to_shell[0]);
 	execlp(argv[i], argv[i], NULL);
-	return errno;
+	return errno; // if execution doesn't work, return error
       }
-      else if(return_code > 0){
-	close(to_shell[1]);
+      else if(return_code > 0){ // if it's the parent
+	close(to_shell[1]); 
 	int pid = return_code;
 	int status = 0;
 	waitpid(pid, &status, 0);
-	if(WEXITSTATUS(status)){
-	  return WEXITSTATUS(status);
+	if(WEXITSTATUS(status)){ // if child's exit status is not 0
+	  return WEXITSTATUS(status); // return error
 	}
 	pipe(to_shell);
-	return_code = fork();
+	return_code = fork(); // create pipe and fork
       }
       else{
-	exit(1);
+	exit(1); // if fork failed, exit
       }
     }
-    else if(i == argc-1){
-      if(return_code == 0){
-	dup2(3, 0);
+    else if(i == argc-1){ // if last argument
+      if(return_code == 0){ // if it's the child
+	dup2(3, 0); // redirect input to read end of pipe (don't worry about stdout)
 	close(to_shell[0]);
 	close(to_shell[1]);
 	execlp(argv[i], argv[i], NULL);
-	return errno;
+	return errno; // if execution doesn't work, return error
       }
-      else if(return_code > 0){
+      else if(return_code > 0){ // if it's the parent
 	int pid = return_code;
 	int status = 0;
 	waitpid(pid, &status, 0);
-	if(WEXITSTATUS(status)){
-	  return WEXITSTATUS(status);
+	if(WEXITSTATUS(status)){ // if child's exit status is not 0
+	  return WEXITSTATUS(status); // return error
 	}
 	close(3);
 	close(to_shell[0]);
-	close(to_shell[1]);
+	close(to_shell[1]); // clean up and don't make pipe/fork
       }
       else{
-	exit(1);
+	exit(1); //if fork failed, exit
       }
     }
-    else{
-      if(return_code == 0){
-	dup2(3, 0);
-	dup2(to_shell[1], 1);
+    else{ // if middle argument
+      if(return_code == 0){ // if it's the child
+	dup2(3, 0); // redirect input to read end of pipe
+	dup2(to_shell[1], 1); // redirect output to write end of pipe
 	close(to_shell[0]);
 	execlp(argv[i], argv[i], NULL);
-	return errno;
+	return errno; // if execution doesn't work, return error
       }
-      else if(return_code > 0){
+      else if(return_code > 0){ // if it's the parent
 	close(3);
 	close(to_shell[1]);
-	dup2(4, 3);
+	dup2(4, 3); // so fd 3 saves read end of previous pipe
 	int pid = return_code;
 	int status = 0;
 	waitpid(pid, &status, 0);
-	if(WEXITSTATUS(status)){
-	  return WEXITSTATUS(status);
+	if(WEXITSTATUS(status)){ // if child's exit status is not 0
+	  return WEXITSTATUS(status); // return error
 	}
 	pipe(to_shell);
-	return_code = fork();
+	return_code = fork(); // create pipe and fork
       }
       else{
-	exit(1);
+	exit(1); // if the fork failed, exit
       }
     }
   }
